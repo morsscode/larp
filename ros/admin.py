29ed0d2e0f198bda012e5ff18416a1683cbb27d3
Riker <9245930+morsscode@users.noreply.github.com>
2023-04-16 16:25:48 -0600

    added models

--- ros/admin.py
+++ ros/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Skill, Spell, Creature, Character
 
 # Register your models here.
+admin.site.register(Skill)
+admin.site.register(Spell)
+admin.site.register(Creature)
+admin.site.register(Character)
--- ros/models.py
+++ ros/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+class Creature(models.Model):
+    name = models.CharField(
+            max_length=200,
+            validators=[MinLengthValidator(2, "Creature must be greater than 1 character")]
+    )
+
+    def __str__(self):
+        return self.name
+
+class Skill(models.Model):
+    name = models.CharField(
+        max_length=50,
+        validators=[MinLengthValidator(2, "Skill must be greater than 1 character")]
+    )
+
+    def __str__(self):
+        return self.name
+
+class Spell(models.Model):
+    name = models.CharField(
+        max_length=50,
+        validators=[MinLengthValidator(2, "spell must be greater than 1 character")]
+    )
+
+    def __str__(self):
+        return self.name
+
+class Character(models.Model):
+    playerName = models.CharField(
+        max_length=50,
+        validators=[MinLengthValidator(2, "Skill must be greater than 1 character")]
+    )
+    CharacterName = models.CharField(
+        max_length=50,
+        validators=[MinLengthValidator(2, "Skill must be greater than 1 character")]
+    )
+    virtue = models.IntegerField(default=50)
+    rep = models.IntegerField(default=50)
+    xp = models.IntegerField(default=0)
+    creatureType = models.ForeignKey(Creature, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.PlayerName
