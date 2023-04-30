f73790efa330bcef5012504d2c2a9576e1287af1
Riker <9245930+morsscode@users.noreply.github.com>
2023-04-30 17:26:22 -0600

    added detail pages for the spells, creatures, and skills that link from the list page

--- ros/urls.py
+++ ros/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'ros'
 urlpatterns = [
     #path ('', views.MainView.as_view(), name='all'),
     path('creatures', views.CreatureListView.as_view(), name='creatures'),
-    #path('creatures/<int:pk>', views.CreatureDetailView.as_view(), name='creature_detail'),
+    path('creatures/<int:pk>', views.CreatureDetailView.as_view(), name='creature_detail'),
     path('spells', views.SpellListView.as_view(), name='spells'),
+    path('spells/<int:pk>', views.SpellDetailView.as_view(), name='spell_detail'),
     path('skills', views.SkillListView.as_view(), name='skills'),
+    path('skills/<int:pk>', views.SkillDetailView.as_view(), name='skill_detail'),
 ]
--- ros/views.py
+++ ros/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from ros.models import Creature
 
 from ros.models import Creature, Spell, Skill
 from ros.owner import OwnerListView, OwnerDetailView
@@ -16,7 +15,43 @@ class CreatureListView(OwnerListView):
     model = Creature
     # By convention:
     # template_name = "ros/creature_list.html"
+
+
+class CreatureDetailView(OwnerDetailView):
+    model = Creature
+    template_name = "ros/creature_detail.html"
+    def get(self, request, pk) :
+        x = Creature.objects.get(id=pk)
+        #comments = Comment.objects.filter(ad=x).order_by('-updated_at')
+        #comment_form = CommentForm()
+        context = {'creature': x}  #'comments': comments, 'comment_form': comment_form }
+        return render(request, self.template_name, context)
+
+class SpellDetailView(OwnerDetailView):
+    model = Spell
+    template_name = "ros/spell_detail.html"
+    def get(self, request, pk) :
+        x = Spell.objects.get(id=pk)
+        #comments = Comment.objects.filter(ad=x).order_by('-updated_at')
+        #comment_form = CommentForm()
+        context = {'spell': x}  #'comments': comments, 'comment_form': comment_form }
+        return render(request, self.template_name, context)
+
+class SkillDetailView(OwnerDetailView):
+    model = Skill
+    template_name = "ros/skill_detail.html"
+    def get(self, request, pk) :
+        x = Skill.objects.get(id=pk)
+        #comments = Comment.objects.filter(ad=x).order_by('-updated_at')
+        #comment_form = CommentForm()
+        context = {'skill': x}  #'comments': comments, 'comment_form': comment_form }
+        return render(request, self.template_name, context)
+
+
+
 class SpellListView(OwnerListView):
     model = Spell
+
+
 class SkillListView(OwnerListView):
     model = Skill
